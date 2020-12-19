4bb1a73f5c17e0773ce8337170766350da16b3f2
andresvarq <66755854+andresvarq@users.noreply.github.com>
2020-12-19 12:17:05 -0500

    pasa pruebas

--- routes/articulo.js
+++ routes/articulo.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const articuloController = require('../controllers/ArticuloController.js');
 const auth = require('../middlewares/auth.js');
 
-router.get('/list', auth.verifyUsuario, articuloController.list);
+router.get('/list', articuloController.list);
 router.post('/add', auth.verifyUsuario, articuloController.add);
 router.put('/update', auth.verifyUsuario, articuloController.update);
 router.put('/activate', auth.verifyUsuario, articuloController.activate);
